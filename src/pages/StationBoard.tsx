936e5be423b30ce8c714bdef05665fff1a1740ea
zack5 <zack5@users.noreply.github.com>
2026-02-24 21:41:56 -0500

    Bugfix to "Loading..." logic

--- src/pages/StationBoard.tsx
+++ src/pages/StationBoard.tsx
@@ -178,7 +178,7 @@ export default function StationBoard({ stationId: propStationId }: StationBoardP
 
   const sortedStopKeys = Object.keys(trains).sort();
 
-  const showLoading = waitingForData && sortedStopKeys.length === 0;
+  const showLoading = activeStationId && waitingForData && sortedStopKeys.length === 0;
   const noAvailableTrains = !showLoading && activeStationId && sortedStopKeys.length === 0
 
   return (
